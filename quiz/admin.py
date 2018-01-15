ecb9349906dc8a5f5918c2b7a6d370f2aff4b7c5
brenda151295 <brenda.solari@ucsp.edu.pe>
2018-01-14 22:55:52 -0500

    Add models

---
+++ quiz/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Pregunta
+
+@admin.register(Pregunta)
+class ClientRequestAdmin(admin.ModelAdmin):
+    list_display = ['enunciado']
---
+++ quiz/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Pregunta(models.Model):
+    enunciado = models.CharField(max_length=256)
+    alternativa_correcta = models.CharField(max_length=256)
+    alternativa_incorrecta_1 = models.CharField(max_length=256)
+    alternativa_incorrecta_2 = models.CharField(max_length=256)
+    alternativa_incorrecta_3 = models.CharField(max_length=256)
+    TEMAS = (
+        ('T1', 'Tema1'),
+        ('T2', 'Tema2'),
+        ('T3', 'Tema3'),
+        ('T4', 'Tema4'),
+    	)
+    tema = models.CharField(max_length=4, choices=TEMAS)
+    imagen = models.ImageField(upload_to='imagenes', null=True, blank=True)
