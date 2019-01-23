9dbeb69917555fc041bf4c39082d0135b4475544
bsolari <brenda151295@gmail.com>
2019-01-23 15:48:12 -0500

    remove heroku

--- mtc_quiz/settings.py
+++ mtc_quiz/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -129,5 +128,3 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
-
-django_heroku.settings(locals())
